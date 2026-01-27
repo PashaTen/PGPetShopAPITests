8b019167b6e6b803c6a7bf6c9b67f3a908110bf1
PashaTen <mamedoff1337@gmail.com>
2026-01-27 21:33:04 +0300

    Добавил тест Попытка удалить несуществующего питомца

---
+++ tests/test_pet.py
@@ -0,0 +1,20 @@
+import allure
+import requests
+
+BASE_URL = "http://5.181.109.28:9090/api/v3"
+
+@allure.feature("pet")
+class TestPet:
+
+    @allure.title("Попытка удалить несуществующего питомца")
+    def test_delete_nonexustent_pet(self):
+        with allure.step("Попытка удалить несуществующего питомца"):
+
+            with allure.step("Отправка запроса на улдаление несуществующего питомца"):
+                response = requests.delete(f"{BASE_URL}/pet/9999")
+
+            with allure.step("Проверка статуса кода"):
+                assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+
+            with allure.step("Проверка текстового содержима ответа"):
+                assert response.text == "Pet deleted", "Текст ошибки не совпал с ожидаемым"
